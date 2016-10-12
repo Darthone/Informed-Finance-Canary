a6f8c160e83237658f80f59d11a0f140b77626df
Anand Patel <ap947@tux4.cs.drexel.edu>
2016-10-12 08:40:57 -0400

    feedparser tutorial

--- feedparser/simpleStuff.py
+++ feedparser/simpleStuff.py
@@ -2,8 +2,24 @@
 import feedparser
 
 def main():
-	d = feedparser.parse("http://feedparser.org/docs/examples/atom10.xml")
-	print(d)
+	# feed data is a dictionary
+	d = feedparser.parse("http://reddit.com/r/python/.rss")
+	# print(d['feed']['title'])
+	
+	# relative links
+	# print d['feed']['link']
 
+	# parse escaped HTML
+	# print d.feed.subtitle
+	
+	# see # of entries
+	print len(d['entries'])
+	print('\n')
+
+	# each entry in the feed is a dictionary. Use [0] to print the first
+	# entry
+	print d['entries'][0]['title']
+
+	# feedparser tutorial
 if __name__ == '__main__':
 	main()
