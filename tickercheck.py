24d9771c36c87c622a61cfb3e62eb1280eb43434
WeiProtein <twk38@drexel.edu>
2017-01-10 01:10:13 -0500

    ticker_check
    checks to see if a ticker is a valid ETF

---
+++ tickercheck.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+
+# Validates if a ticker is ETF (Exchange Traded Fund)
+
+import urllib2
+
+def request(symbol, tag):
+	url = 'http://finance.yahoo.com/d/quotes.csv?s=%s&f=%s' % (symbol, tag)
+	response = urllib2.urlopen(url)
+	content = response.read().decode().strip().strip('"')
+	return content
+
+def get_name(symbol):
+  return request(symbol, 'n')
+
+def valid_ticker(ticker):
+	ticker = ticker.upper()
+	print get_name(ticker)
+	return True if get_name(ticker) != 'N/A' else False
+
+print valid_ticker('UA')
