8ad1682f09da42ebd3881d511204a103a494c66c
Dario Marasco <dm661@live.com>
2017-01-25 11:17:21 -0500

    script that checks a body of text for any explictly stated stock tickers

--- historic_gather/gather.py
+++ historic_gather/gather.py
@@ -18,27 +18,39 @@ def create_urls(filename):
     for date in json_data['dates']:
         ret.append(url_template % (date))
     return ret
-
-def article_to_file(url, folder):
+            
+def article_to_file(url, folder, main_url):
     j = {}
     try:
         a = Article(url)
         a.download()
         a.parse()
+        j['url'] = url
         j['title'] = a.title.replace('/', '\\')
         j['text'] = a.text
         j['authors'] = a.authors
-        j['date'] = a.publish_date.strftime("%Y%m%d")
-        j['epoch'] = a.publish_date.strftime("%s") # to epoch time
+        j['error'] = None
+        try:
+            if "An Error has occured" in a.title:
+                print "Bad article (no title): ", url
+                return
+            j['date'] = a.publish_date.strftime("%Y%m%d")
+            j['epoch'] = a.publish_date.strftime("%s") # to epoch time
+        except Exception as e:
+            print "Bad article (Date): ", url
+            j['date'] = '' 
+            j['epoch'] = ''
         a.nlp()
         j['keywords'] = a.keywords
         filename = os.path.join(folder, "%s_%s" % (j['date'], j['title']))
 
-        print j['date'], a.title 
+        #print j['date'], a.title 
+        
         with open(filename, 'w') as f:
             f.write(json.dumps(j))
+            
     except Exception as e:
-        print e
+        print "Bad article: ", url
 
 def url_to_articles(url):
     ret = []
@@ -49,32 +61,39 @@ def url_to_articles(url):
 
 q = Queue()
 folder = "articles"
-num_worker_threads = 18
+num_worker_threads = 4
 
 def dl_worker():
-    url = q.get()
-    if url is not None:
-        for u in url_to_articles(url):
-            article_to_file(u, folder)
-        q.task_done()
+    while not q.empty():
         url = q.get()
+        try:
+            for u in url_to_articles(url):
+                try:
+                    article_to_file(u, folder, url)
+                except Exception as e:
+                    print e, u
+        except Exception as e:
+            print e, u
+
     q.task_done()
+
 
 def main():
     urls = create_urls("articles.json")
     if not os.path.exists(folder) or not os.path.isdir(folder):
         os.mkdir(folder)
 
+    urls.reverse()
+    #for u in urls[1800:]:
+    for u in urls[:]:
+        q.put(u)
+
     for i in range(num_worker_threads):
         t = Thread(target=dl_worker)
         t.daemon = True
         t.start()
 
-    #urls.reverse()
-    for u in urls:
-        q.put(u)
-     
-    q.join()
+    q.join() # wait for threads
 
 if __name__ == "__main__": 
     nltk.download('punkt')
