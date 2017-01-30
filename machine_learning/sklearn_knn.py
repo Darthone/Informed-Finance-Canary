b324cfd98be214993f227d4f1077b2201bc67fb3
weiprotein <twk38@drexel.edu>
2017-01-29 21:53:33 -0500

    changed knn to refelct discrete up/down daily return

--- machine_learning/sklearn_knn.py
+++ machine_learning/sklearn_knn.py
@@ -2,59 +2,126 @@ import numpy as np
 import pandas as pd
 from sklearn import preprocessing, cross_validation, neighbors 
 
-def k_nearest_neighbors(data, predict, k=3.0):
-	if len(data) >= k:
-		warnings.warn('K is a value less than total groups')
-
-	distances = []
-	for group in data:
-		for features in data[group]:
-			#calculating euclidian distance
-			e_distance = np.linalg.norm(np.array(features)-np.array(predict))
-			distances.append([e_distance, group])
-
-	votes = [i[1] for i in sorted(distances)[:k]]
-	#print(Counter(votes).most_common(1))
-	vote_result = Counter(votes).most_common(1)[0][0]
-	confidence = float(Counter(votes).most_common(1)[0][1]) / k
-	
-	#print(vote_result, confidence)
-
-	#knnalgos
-	return vote_result, confidence
-
-accuracies = []
-
-for i in range(5):
-	df = pd.read_csv("AAPL.txt")
-	#df.replace('?',-99999, inplace=True)
-	df.drop(['date'], 1, inplace=True)
-	df.drop(['low'], 1, inplace=True)
-	df.drop(['volume'], 1, inplace=True)
-
-	X = np.array(df.drop(['open'],1))
-	y = np.array(df['open'])
-
-	X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, y, test_size=0.2)
-
-	clf = neighbors.KNeighborsRegressor() 
-	clf.fit(X_train,y_train)
-
-	accuracy = clf.score(X_test,y_test)
-
+def addDailyReturn(dataset):
 	"""
-	print correct
-	print total 
-	print percent_correct
+	Adding in daily return to create binary classifiers (Up or Down in relation to the previous day)
 	"""
 
-	accuracies.append(accuracy)
+	#will normalize labels
+	le = preprocessing.LabelEncoder()
 
-print(sum(accuracies)/len(accuracies))
+	dataset['UpDown'] = (dataset['close']-dataset['close'].shift(-1))/dataset['close'].shift(-1)
+	dataset.UpDown[dataset.UpDown >= 0] = 'Up'
+	dataset.UpDown[dataset.UpDown < 0] = 'Down'
+	dataset.UpDown = le.fit(dataset.UpDown).transform(dataset.UpDown)
+
+"""
+	features = dataset.columns[1:-1]
+	X = dataset[features]    
+	y = dataset.UpDown    
+
+	X_train = X[X.index < start_test]
+	y_train = y[y.index < start_test]              
+
+	X_test = X[X.index >= start_test]    
+	y_test = y[y.index >= start_test]
+
+	return X_train, y_train, X_test, y_test   
+"""
+
+df = pd.read_csv("AAPL.txt")
+#using open, high, close to determine UpDown
+df.drop(['date'], 1, inplace=True)
+df.drop(['low'], 1, inplace=True)
+df.drop(['volume'], 1, inplace=True)
+df.drop(['open'], 1, inplace=True)
+
+addDailyReturn(df)
+
+X = np.array(df.drop(['UpDown'],1))
+y = np.array(df['UpDown'])
+print y
+
+X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, y, test_size=0.2)
+
+clf = neighbors.KNeighborsClassifier() 
+clf.fit(X_train,y_train)
+
+accuracy = clf.score(X_test,y_test)
+
+print accuracy
 
 test_set = np.array([[104,106]])
 
-test_set = test_set.reshape(len(test_set),-1)
+ = test_seetest_setreDataForClassification(dataset, start_test):
+    """
+    generates categorical output column, attach to dataframe 
+    label the categories and split into train and test
+    """
+    le = preprocessing.LabelEncoder()
+    
+    dataset['UpDown'] = dataset['Return_Out']
+    dataset.UpDown[dataset.UpDown >= 0] = 'Up'
+    dataset.UpDown[dataset.UpDown < 0] = 'Down'
+    dataset.UpDown = le.fit(dataset.UpDown).transform(dataset.UpDown)
+    
+    features = dataset.columns[1:-1]
+    X = dataset[features]    
+    y = dataset.UpDown    
+    
+    X_train = X[X.index < start_test]
+    y_train = y[y.index < start_test]              
+    
+    X_test = X[X.index >= start_test]    
+    y_test = y[y.index >= start_test]
+    
+    return X_train, y_train, X_test, y_test   
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
+22
+23
+def prepareDataForClassification(dataset, start_test):
+    """
+    generates categorical output column, attach to dataframe 
+    label the categories and split into train and test
+    """
+    le = preprocessing.LabelEncoder()
+    
+    dataset['UpDown'] = dataset['Return_Out']
+    dataset.UpDown[dataset.UpDown >= 0] = 'Up'
+    dataset.UpDown[dataset.UpDown < 0] = 'Down'
+    dataset.UpDown = le.fit(dataset.UpDown).transform(dataset.UpDown)
+    
+    features = dataset.columns[1:-1]
+    X = dataset[features]    
+    y = dataset.UpDown    
+    
+    X_train = X[X.index < start_test]
+    y_train = y[y.index < start_test]              
+    
+    X_test = X[X.index >= start_test]    
+    y_test = y[y.index >= start_test]
+    
+    return X_train, y_train, X_test, y_test   test_set.reshape(len(test_set),-1)
 
 prediction = clf.predict(test_set)
 
