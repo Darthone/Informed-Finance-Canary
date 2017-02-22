aa73bf13634412d88a9d823bc0a95c7558519e37
Dario Marasco <dm661@live.com>
2017-02-21 20:19:05 -0500

    clean up, please don't commit articles

--- ifc/historic_gather/dates.sh
+++
@@ -1,9 +0,0 @@
-startdate=20070101
-enddate=`date +%Y%m%d`
-
-curr="$startdate"
-while true; do
-    echo "$curr"
-    [ "$curr" \< "$enddate" ] || break
-    curr=$( date +%Y%m%d --date "$curr +1 day" )
-done
