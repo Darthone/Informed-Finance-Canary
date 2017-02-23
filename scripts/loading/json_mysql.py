8b1e4dcff83a575e2784e093a5701192acfb12ff
Trey Harper <tsuns4life@gmail.com>
2017-02-22 22:24:42 -0500

    Added stock names

--- scripts/loading/json_mysql.py
+++ scripts/loading/json_mysql.py
@@ -8,6 +8,8 @@ filterwarnings('ignore', category = mdb.Warning)
 import sys
 from pprint import pprint
 from shutil import move
+import requests
+from lxml import html
 
 def get_files(file_path):
     """ returns all files in a given file_path"""
@@ -16,9 +18,20 @@ def get_files(file_path):
         for f in files:
             ret.append(os.path.join(root, f))
     return ret
+	
+def stock_name(ticker):
+	sym = "symbol=" + ticker
+	url_base = "http://www.reuters.com/finance/stocks/overview?" + sym
+	ret = []
+	page = requests.get(url_base)
+	tree = html.fromstring(page.content)
+	ret = tree.xpath('//div[@id="sectionTitle"]/h1/text()')
+	stock_name = ''.join(ret)
+	formatted_stock_name = stock_name.split(" (")[0]
+	return formatted_stock_name
 
 def insert_article_file(file_path, ingested_path):
-	con = mdb.connect()
+	con = mdb.connect('192.168.1.128','trey','securePassword17!','ifc',3306, charset='utf8')
 	with con:
 		with open(file_path, 'r') as data_file:
 			data = json.load(data_file)
@@ -35,9 +48,9 @@ def insert_article_file(file_path, ingested_path):
 			
 			if 'tickers' in data:
 				tickers = data['tickers']
-				tickerArgs = [(tickers,tickers) for tickers in data['tickers']] 
-				tickerQuery = 'INSERT IGNORE INTO stocks(stocks_id,ticker) VALUES (%s,%s)'
-				cur.executemany(tickerQuery,tickerArgs)
+				tickerArgs = [(tickers,tickers,stock_name(tickers)) for tickers in data['tickers']]
+				tickerQuery = 'INSERT IGNORE INTO stocks(stocks_id,ticker,name) VALUES (%s,%s,%s)'
+				cur.executemany(tickerQuery, tickerArgs)
 
 			title = data["title"]
 			date = data["date"]
@@ -50,19 +63,17 @@ def insert_article_file(file_path, ingested_path):
 			
 		finally:
 			cur.close()
-			if not os.path.exists(file_path):
+			if os.path.exists(file_path):
 				move(file_path,ingested_path)
-			else:
-				os.remove(file_path)
 		
 
 def main():
 	# Directory to where articles are stored
-	articles_path = u"/home/trey/bug-free-octo-parakeet/rss_grabber/handled/test"
+	articles_path = u"/home/trey/bug-free-octo-parakeet/rss_grabber/handled/stock"
 	# Gets each filename
 	files = get_files(articles_path)
 	# Directory where ingested files will be moved to
-	ingested_path = u"/home/trey/bug-free-octo-parakeet/rss_grabber/handled/testIngest"
+	ingested_path = u"/home/trey/bug-free-octo-parakeet/rss_grabber/handled/ingested"
 	
 	for f in files:
 		insert_article_file(f,ingested_path)
