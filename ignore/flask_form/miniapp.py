9a196ac6c126b47512d1c016a0c9b210f459c313
anandpatel23 <ap947@drexel.edu>
2017-04-11 13:29:46 -0400

    all user fields required

--- ignore/flask_form/miniapp.py
+++ ignore/flask_form/miniapp.py
@@ -11,6 +11,11 @@ app.config['SECRET_KEY'] = '7d441f27d441f27567d441f2b6176a'
 class ReusableForm(Form):
 	tickerName = TextField('Ticker Name:', validators=[validators.required()])
 	sma1 = TextField('Simple Moving Average 1 (SMA) (eg 10):', validators=[validators.required()])
+	sma2 = TextField('Simple Moving Average 2 (SMA) (eg 30):', validators=[validators.required()])
+	dateRange = TextField('Length of Process: ', validators=[validators.required()])
+	nslow = TextField('Slow EMA (eg 26): ', validators=[validators.required()])
+	nfast = TextField('Fast EMA (eg 12): ', validators=[validators.required()])
+	nema = TextField('EMA Signal Line (eg 9): ', validators=[validators.required()])
 
 @app.route("/", methods=['GET', 'POST'])
 def hello():
@@ -20,11 +25,21 @@ def hello():
 	if request.method == 'POST':
 		tickerName = request.form['tickerName']
 		sma1 = request.form['sma1']
-		print tickerName, sma1
+		sma2 = request.form['sma2']
+		dateRange = request.form['dateRange']
+		nslow = request.form['nslow']
+		nfast = request.form['nfast']
+		nema = request.form['nema']
+		print tickerName, sma1, sma2, dateRange, nslow, nfast, nema
 		
 		if form.validate():
 			flash('Ticker Name: ' + tickerName)
 			flash('Simple Moving Average 1 (SMA) (eg 10): ' + sma1)
+			flash('Simple Moving Average 2 (SMA) (eg 30): ' + sma2)
+			flash('Length of Process: ' + dateRange)
+			flash('Slow EMA (eg 26): ' + nslow)
+			flash('Fast EMA (eg 12): ' + nfast)
+			flash('EMA Signal Line (eg 9): ' + nema)
 		else:
 			flash('Error: All the form fields are required. ')
 
