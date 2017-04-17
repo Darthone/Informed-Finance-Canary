7c891affe8f81ae48167d6b6b9682bdf11206579
Dario Marasco <dm661@live.com>
2017-04-17 00:40:35 -0400

    fixing up stock gathering info

--- ifc/stockData.py
+++ ifc/stockData.py
@@ -1,15 +1,37 @@
 #!/usr/bin/env python
-from yahoo_finance import Share
+from common import memoize
+#from DateTime import date, timedelta
+#from ifc.db import StockFeature, database
 
-def get_data_for_sym(ticker_sym, start, end):
-    #TODO Error catching
+import logging
+#import peewee
+import yahoo_finance
+
+def get_data_for_sym_from_yahoo(ticker_sym, start, end):
     """ returns a list of dicts for stock data formatted as:
             {u'Volume': u'28720000', u'Symbol': u'YHOO', u'Adj_Close': u'35.83', u'High': u'35.89', u'Low': u'34.12', u'Date': u'2014-04-29', u'Close': u'35.83', u'Open': u'34.37'}
         Dates must be in format of YYYY-mm-dd """
+    try:
+        return yahoo_finance.Share(ticker_sym).get_historical(start, end)
+    except yahoo_finance.YQLResponseMalformedError:
+        logging.error("Malformed Url")
+        return []
 
-    #TODO check if db has data
-    # if not grab data and put it into the DB
-    symbol = Share(ticker_sym)
-    return symbol.get_historical(start, end)
+#@memoize
+def get_data_for_sym(ticker_sym, start, end):
+    return get_data_for_sym_from_yahoo(ticker_sym, start, end)
+	#res = StockFeature.select().where(Relationship.from_user == self))
 
-
+"""
+def fill_db_with_data(ticker_sym, days_back=3650):
+    start = date.today()
+    end = date.today() - timedelta(days_back)
+    EndDate = Yesterday.strftime("%Y-%m-%d")
+	data = get_data_for_sym(ticker_sym, star.tstrftime("%Y-%m-%d"), end.strftime("%Y-%m-%d"))
+	db_data = ({'stock_id': li["Symbol"], 'date': li["Date"], 'high': li["High"], 'low': li["Low"], 'volume': li["Volume"], 'opening': li["Open"], 'closing': li["Close"]} for li in data)
+	try:
+		with database.atomic():1
+			StockFeature.insert_many(db_data).execute()
+ 	except peewee.IntegrityError:
+		logging.info('Skipping Duplicate')
+        """
