548cbef6403bc42b4231313398c297d5ca49ee83
anandpatel23 <ap947@drexel.edu>
2017-04-17 22:17:27 -0400

    testing web int

--- web_interface/miniapp.py
+++ web_interface/miniapp.py
@@ -45,5 +45,99 @@ def hello():
 
 	return render_template('index.html', form=form)
 
+def pullData(stock, stockRange):
+	# none or hold for stance
+	stance = 'none'
+	priceBought = 0
+	priceSold = 0
+	pricePrevious = 0
+	totalProfit = 0
+	tradeCount = 0
+	startingPrice = 0
+
+	try:
+		# values format -> date, close, high, low, open, volume
+		urlToVisit = 'http://chartapi.finance.yahoo.com/instrument/1.0/' + stock + '/chartdata;type=quote;range=' + stockRange + '/csv'
+		f = stock + '.txt'
+
+		try:
+			# if the file exists, open file and grab latest unix timestamp
+			existingFile = open(f,'r').read()
+			splitExisting = existingFile.split('\n')
+			mostRecentLine = splitExisting[-2]
+			lastUnix = mostRecentLine.split(',')[0]
+
+		except Exception,e:
+			flash('error in inner try:', str (e))
+			lastUnix = 0
+
+		# append new lines from site to existing file
+		saveFile = open(f, 'a')
+		openedSite = urllib2.urlopen(urlToVisit).read()
+		splitSource = openedSite.split('\n')
+
+		# grab valid lines after values section
+		try:
+			for l in splitSource:
+				if l[0].isdigit():
+					l_split = l.split(',')
+					price =  float(l_split[1])
+					currentDate = str(l_split[0])
+					currentDateObj = datetime.datetime.strptime(currentDate, '%Y%m%d')
+					flash('Date: ' + currentDateObj.strftime('%m %d, %Y'))
+					if 'values' not in l:
+						splitLine = l.split(',')
+						if len(splitLine) == 6:
+							# most recent time? append to file
+							if int(splitLine[0]) > int(lastUnix):
+								lineToWrite = l + '\n'
+								saveFile.write(lineToWrite)
+					flash('Price: ', price)
+
+					# trading stances:
+					# buy when not invested and stock price drops
+					# sell when price is > .2% higher than bought
+					if stance == 'none':
+						if price < pricePrevious:
+							flash('buy triggered')
+							priceBought = price
+							flash('bought stock for ', priceBought)
+							stance = 'holding'
+							if tradeCount == 0:
+								startingPrice = priceBought
+							tradeCount += 1
+					elif stance == 'holding':
+						if price > (priceBought * .002 + priceBought):
+							flash('sell triggered')
+							priceSold = price
+							flash('finished trade, sold for: ', priceSold)
+							stance = 'none'
+							tradeProfit = priceSold - priceBought
+							totalProfit += tradeProfit
+							flash(totalProfit)
+							tradeCount += 1
+					pricePrevious = price
+
+				flash('Gross Profit Per Stock:', totalProfit)
+				flash('# of Trades:', tradeCount)
+				flash('------------------------------------')				
+
+				try:
+					grossPercentProfit = totalProfit/startingPrice * 100
+					flash('Gross percent profit:', grossPercentProfit)
+				except ZeroDivisionError:
+					pass
+		except IndexError:
+			pass
+
+		saveFile.close()
+
+		flash('Pulled', stock)
+		flash(str(datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%S')))
+
+	except Exception, e:
+		flash('error in main:', str(e))
+
+
 if __name__ == "__main__":
 	app.run(host='0.0.0.0', port=8000)
