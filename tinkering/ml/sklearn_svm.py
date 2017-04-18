93effbd6e2d98d7707946f1248e19bd37da57105
weiprotein <twk38@drexel.edu>
2017-04-17 23:21:13 -0400

    changed data source to db

--- tinkering/ml/sklearn_svm.py
+++ tinkering/ml/sklearn_svm.py
@@ -1,6 +1,12 @@
+#!/usr/bin/env python
 import numpy as np
 import pandas as pd
 from sklearn import preprocessing, cross_validation, neighbors, svm 
+import peewee
+from peewee import *
+import ifc.ta as ta
+
+database = MySQLDatabase("ifc", host="192.168.1.128", port=3306, user="", passwd="")
 
 def addDailyReturn(dataset):
 	"""
@@ -10,7 +16,7 @@ def addDailyReturn(dataset):
 	#will normalize labels
 	le = preprocessing.LabelEncoder()
 
-	dataset['UpDown'] = -(dataset['adj_close']-dataset['adj_close'].shift(-1))/dataset['adj_close'].shift(-1)
+	dataset['UpDown'] = -(dataset['Adj_Close']-dataset['Adj_Close'].shift(-1))/dataset['Adj_Close'].shift(-1)
 	print dataset['UpDown']
 	dataset.UpDown[dataset.UpDown >= 0] = 'Up' 
 	dataset.UpDown[dataset.UpDown < 0] = 'Down'
@@ -31,23 +37,30 @@ def addDailyReturn(dataset):
 """
 accuracies = []
 
-def preProcessing(csv):
-	df = pd.read_csv(csv)
+def preProcessing(start_date, end_date):
+	x = ta.get_series("TGT", start_date, end_date)
+	x.run_calculations()                            
+	x.trim_fat()                                    
+	df = x.df
+	#df = pd.read_csv(csv)
 	addDailyReturn(df)
 	
-	df.drop(['date'], 1, inplace=True)
-	df.drop(['low'], 1, inplace=True)
-	df.drop(['volume'], 1, inplace=True)
+	df.drop(['Date'], 1, inplace=True)
+	df.drop(['Low'], 1, inplace=True)
+	df.drop(['Volume'], 1, inplace=True)
 	#df.drop(['open'], 1, inplace=True)
-	df.drop(['adj_close'],1, inplace=True)
+	df.drop(['Adj_Close'],1, inplace=True)
 	#df.drop(['close'],1, inplace=True)
-	df.drop(['high'],1, inplace=True)
+	df.drop(['High'],1, inplace=True)
+	df.drop(['mavg_10'],1, inplace=True)
+	df.drop(['mavg_30'],1, inplace=True)
+	df.drop(['rsi_14'],1, inplace=True)
 	
 	return df
 
-for i in range(100):
-	train_df = preProcessing('GM_14_15.csv')
-	test_df = preProcessing('GM_16_17.csv')
+for i in range(3):
+	train_df = preProcessing("2015-04-17", "2016-04-17")
+	test_df = preProcessing("2016-04-17", "2017-04-17")
 
 	X_train = np.array(train_df.drop(['UpDown'],1))
 	y_train = np.array(train_df['UpDown'])
@@ -56,7 +69,7 @@ for i in range(100):
 	
 	#X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, y, test_size=0.5)
 
-	clf = svm.SVC() 
+	clf = svm.SVC()
 	clf.fit(X_train,y_train)
 
 	accuracy = clf.score(X_test,y_test)
