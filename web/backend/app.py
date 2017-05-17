417dc16ff77b758f26581cd88a32841d1f213c53
Dario Marasco <dm661@live.com>
2017-05-17 10:23:34 -0400

    initial backend commit

---
+++ web/backend/app.py
@@ -0,0 +1,129 @@
+#!/usr/bin/env python
+
+import ujson as json
+
+import pandas as pd
+import random # for spoofing
+
+from datetime import datetime
+from flask import Flask
+from flask_restful import reqparse, abort, Api, Resource
+from flask_cors import CORS
+from playhouse.shortcuts import model_to_dict
+
+
+from ifc.ta import get_series
+from ifc.db import *
+
+app = Flask(__name__)
+CORS(app)
+api = Api(app)
+
+stock_date_range_parser = reqparse.RequestParser()
+stock_date_range_parser.add_argument('symbol')
+stock_date_range_parser.add_argument('start')
+stock_date_range_parser.add_argument('end')
+
+
+class Articles(Resource):
+    """ returns related articles"""
+    def get(self):
+        args = stock_date_range_parser.parse_args()
+        sym = Stock.get(ticker=args['symbol'])
+        articles = StockArticle.select().join(Article).where(StockArticle.stock == sym, 
+                           Article.date > datetime.strptime(args['start'], "%Y-%m-%d").date(), 
+                           Article.date < datetime.strptime(args['end'], "%Y-%m-%d").date()).order_by(Article.date)
+        ret = {'data':[]}
+        tmp = []
+        dup = {}
+        for a in articles:
+            if not dup.has_key(a.article.title): # this is a hack, dupes should not exist in the first place
+                f = [x for x in ArticleFeature.select().join(Article).where(Article.title == a.article.title)]
+                print "here", dir(f), f
+                item = {
+                    "author": a.article.author.name,
+                    "date": a.article.date.strftime("%Y-%m-%d"),
+                    "title": a.article.title,
+                    "content": a.article.content,
+                    "source": a.article.source,
+                    "keywords": [],
+                    "sentiment": {}
+                }
+                if len(f) == 1:
+                    f = f[0]
+                    item["sentiment"] = {
+                        "pos": float(f.positive) * 10,
+                        "net": float(f.neutral) * 10,
+                        "neg": float(f.negative) * 10,
+                        "comp": float(f.compound) * 10
+                        }
+                tmp.append(item)
+                dup[a.article.title] = ""
+        ret['data'] = list(reversed(tmp))
+        return ret
+
+def get_summary(start, end, sym):
+    ret = {}
+    return ret
+
+def get_backtesting(start, end, data):
+    ret = {
+        "price": data.df['Adj_Close'][len(data.df) - 1],
+        "indicator": random.randint(1,5),
+        "gross": 0.0,
+        "numTrades": 1,
+        "grossPercent": 5.8,
+        }
+    return ret
+
+class StockData(Resource):
+    """ returns stock data and calculations"""
+    def get(self):
+        stock_date_range_parser.add_argument('params')
+        args = stock_date_range_parser.parse_args()
+        calcs = json.loads(args['params'])
+
+        x = get_series(args['symbol'], args['start'], args['end'])
+        #x.run_calculations()
+        cc = []
+        for calc in calcs:
+            t = calc['type']
+            p = calc['param']
+            if t == 'mavg':
+                cc.append(x.calculate_mavg(p['window']))
+            elif t == 'rsi':
+                cc.append(x.calculate_rsi(p['window']))
+            elif t == 'macd':
+                cc.append('signal_' + str(p['signal']))
+                cc.append(x.calculate_macd(**p))
+            elif t == '':
+                pass
+                #TODO
+                #x.calculate_mom()
+                #x.calculate_rocr()
+                #x.calculate_atr()
+                #x.calculate_mfi()
+                #x.calculate_obv()
+                #x.calculate_cci()
+                #x.calculate_trix()
+                #x.calculate_adx()
+        x.df = x.df.where((pd.notnull(x.df)), None) # cast Nan to Null
+        jret = {
+            'ohlc' : list(zip(x.df.epoch, x.df['Open'], x.df['High'], x.df['Low'], x.df['Adj_Close'])),
+            'volume': list(zip(x.df.epoch, x.df['Volume'])),
+            'columns': cc,
+            'backtesting': get_backtesting(args['start'], args['end'], x)
+        }
+        for c in cc:
+            jret[c] = list(zip(x.df.epoch, x.df[c]))
+        return jret 
+
+#
+# Actually setup the Api resource routing here
+#
+api.add_resource(Articles, '/articles')
+api.add_resource(StockData, '/stock')
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host="0.0.0.0", port=5001)
